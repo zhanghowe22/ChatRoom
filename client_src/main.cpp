a5ccccac84096dfd82a1b80825f615d615f37090
zhanghowe <zhanghowe122@gmail.com>
2024-08-30 15:11:46 +0800

    chatClient: 调整客户端界面

--- client_src/main.cpp
+++ client_src/main.cpp
@@ -1,11 +1,10 @@
-#include "mainwindow.h"
-
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+#include "chatClient.h"
+
+int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    Client client;
+    client.show();
     return a.exec();
 }
